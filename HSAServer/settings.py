552174d503f0124c8af8d57cb983adf02edac659
Hossein Noroozpour <Hossein.Noroozpour@gmail.com>
2015-01-18 13:26:04 +0330

    heroku

--- HSAServer/settings.py
+++ HSAServer/settings.py
@@ -82,8 +82,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-
-os.environ['wsgi.url_scheme'] = 'https'
